cf6b285f4935322ee2c538ae45b97b798488db93
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2021-09-29 12:20:19 -0500

    Updated folders for submissions and slides

--- submissions/c_dictionaries/dictionaries_diegopajarito.py
+++ submissions/c_dictionaries/dictionaries_diegopajarito.py
@@ -1,3 +1,16 @@
 cities = {'last_1': 'Bogota', 'last_2': 'Duitama', 'last_3': 'Medellin', 'last_4': 'Barcelona', 'last_5': 'Lisbon'}
 
 print(cities)
+
+cities = { 'Barcelona': {'population': 5474482, 'unemployment_rate': 17.24},
+           'lisbon': {'population': 2827514, 'unemployment_rate': 7.4},
+           'Amsterdam': {'population': 2431000, 'unemployment_rate': 3.3} }
+
+
+cities['Bogota'] = {'population': 7450987, 'unemployment_rate': 11.3}
+
+print(cities)
+
+keys = cities.keys()
+for city in keys:
+    print(cities[city])
