ed41afd34c51a9f804e63ae744fe316ef07cc606
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2021-09-29 11:18:33 -0500

    Updated folders for submissions and slides

---
+++ submissions/b_lists/lists_diegopajarito.py
@@ -0,0 +1,9 @@
+my_list = [21, 22, 23]
+
+print('Looping a list using indexes')
+for i in range(len(my_list)):
+    print(my_list[i])
+
+print('looping a list items')
+for item in my_list:
+    print(item)
---
+++ submissions/c_dictionaries/dictionaries_diegopajarito.py
@@ -0,0 +1,3 @@
+cities = {'last_1': 'Bogota', 'last_2': 'Duitama', 'last_3': 'Medellin', 'last_4': 'Barcelona', 'last_5': 'Lisbon'}
+
+print(cities)
